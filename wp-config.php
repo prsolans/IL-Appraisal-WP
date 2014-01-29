7191d0a41c54a01064f05572b0930f1401ef59dc
prsolans <paul@mergeworld.com>
2014-01-29 00:16:35 -0600

    Added Dev environment to the repo. Uploaded images and data

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@ define('DB_NAME', 'illinois_appraisal');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '0.0.0.0');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
